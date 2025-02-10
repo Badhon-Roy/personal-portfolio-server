9c9930051dc824444cdc58f2dc1500e0170665e3
Badhon-Roy <roybadhon286@gmail.com>
2025-02-10 23:27:02 +0600

    CRED operation done for blogs

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
 
 
 
-  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wj0pjif.mongodb.net/?retryWrites=true&w=majority`;
   
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,12 +38,69 @@ app.use(cors({
       await client.connect();
   
   
-  
-      // book category related api
-      app.get('/projects', async (req, res) => {
+
+          // API routes for projects
+    app.get('/projects', async (req, res) => {
+      try {
         const result = await projectsCollection.find().toArray();
-        res.send(result)
-      })
+        res.status(200).send(result);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+
+    // Blog APIs
+
+    // Create Blog
+    app.post("/blogs", async (req, res) => {
+      try {
+        const newBlog = req.body; // Assuming the blog data is passed directly in the request body
+        const result = await blogCollection.insertOne(newBlog);
+        res.status(201).json(result.ops[0]); // Return the created blog
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+
+    // Get All Blogs
+    app.get("/blogs", async (req, res) => {
+      try {
+        const blogs = await blogCollection.find().toArray();
+        res.status(200).json(blogs);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+
+    // Get Single Blog by ID
+    app.get("/blogs/:id", async (req, res) => {
+      try {
+        const blog = await blogCollection.findOne({ _id: new ObjectId(req.params.id) });
+        if (!blog) {
+          return res.status(404).json({ error: 'Blog not found' });
+        }
+        res.status(200).json(blog);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+
+    // Update Blog
+    app.put("/blogs/:id", async (req, res) => {
+      try {
+        const updatedBlog = await blogCollection.findOneAndUpdate(
+          { _id: new ObjectId(req.params.id) },
+          { $set: req.body },
+          { returnDocument: 'after' } // This returns the updated document
+        );
+        if (!updatedBlog.value) {
+          return res.status(404).json({ error: 'Blog not found' });
+        }
+        res.status(200).json(updatedBlog.value);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
   
   
 
